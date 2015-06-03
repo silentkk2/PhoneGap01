1f6941718824782aa7fd5d0577766b85794c6af8
silentkk2 <aminur.rahman.79@my.csun.edu>
2015-06-02 22:15:20 -0700

    toggle flashlight

--- www/js/index.js
+++ www/js/index.js
@@ -63,6 +63,22 @@ var app = {
 
         var watcher01 = navigator.compass.watchHeading(onSuccessCompass, onErrorCompass, options);
 
+        window.plugins.flashlight.available(function(isAvailable) {
+          if (isAvailable) {
+
+            // switch on
+            window.plugins.flashlight.switchOn(); // success/error callbacks may be passed
+
+            // switch off after 3 seconds
+            setTimeout(function() {
+              window.plugins.flashlight.switchOff(); // success/error callbacks may be passed
+            }, 3000);
+
+          } else {
+            alert("Flashlight not available on this device");
+          }
+        });
+
     }
 
 
